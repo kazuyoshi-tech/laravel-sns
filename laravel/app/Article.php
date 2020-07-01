fef9a319608a8ce53bdb370808a687c73d00feaa
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-07-01 18:47:21 +0900

    投稿編集機能実装完了

--- laravel/app/Article.php
+++ laravel/app/Article.php
@@ -7,7 +7,13 @@ use Illuminate\Database\Eloquent\Ralations\BelongsTo;
 
 class Article extends Model
 {
-    public function user(): BelongsTo {
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+
+    public function user()
+    {
         return $this->belongsTo('App\User');
     }
 }
--- laravel/resources/views/nav.blade.php
+++ laravel/resources/views/nav.blade.php
@@ -8,17 +8,17 @@
         <li class="nav-item">
             <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
         </li>
-        @endquest
+        @endguest
 
         @guest 
         <li class="nav-item">
             <a class="nav-link" href="{{ route('login') }}">ログイン</a>
         </li>
-        @endquest
+        @endguest
 
         @auth
         <li class="nav-item">
-            <a class="nav-link" href=""><i class="fas fa-pen mr-1"></i>投稿する</a>
+            <a class="nav-link" href="{{ route('articles.create') }}"><i class="fas fa-pen mr-1"></i>投稿する</a>
         </li>
         @endauth
 
@@ -40,7 +40,7 @@
                 </button>
             </div>
         </li>
-            <form id="logout-button" method="POST" action="{{ route('logout')}}">@csrf
+        <form id="logout-button" method="POST" action="{{ route('logout')}}">@csrf
         </form>
         <!-- Dropdown -->
         @endauth
