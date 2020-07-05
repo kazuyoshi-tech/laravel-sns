bef3e1e4ad4f9f977f1124f0d17c7be9d3e29d14
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-07-05 17:31:11 +0900

    ユーザーページに投稿一覧表示完了

--- laravel/resources/views/users/show.blade.php
+++ laravel/resources/views/users/show.blade.php
@@ -11,30 +11,49 @@
                     <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
                         <i class="fas fa-user-circle fa-3x"></i>
                     </a>
-                    @if(Auth::id() !== $user->id)
-                        <follow-button
-                            class="ml-auto"
-                            :initial-is-followed-by='@json($user->isFollowedBy(Auth::user()))'
-                        >
-                        </follow-button>
-                    @endif
                     <h2 class="h5 card-title m-0">
                         <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
                             {{ $user->name }}
                         </a>
                     </h2>
+                    @if(Auth::id() !== $user->id)
+                        <follow-button
+                            class="ml-auto"
+                            :initial-is-followed-by='@json($user->isFollowedBy(Auth::user()))'
+                            :authorized='@json(Auth::check())'
+                            endpoint="{{ route('users.follow', ['name' => $user->name]) }}"
+                        >
+                        </follow-button>
+                    @endif
                 </div>
             </div>
             <div class="card-body">
                 <div class="card-text">
                     <a href="" class="text-muted">
-                        10フォロー
+                        {{ $user->count_followings }}　フォロー
                     </a>
                     <a href="" class="text-muted">
-                        10フォロわー
+                        {{ $user->count_followers }} フォロワー
                     </a>
                 </div>
             </div>
         </div>
+        <ul class="nav nav-tabs nav-justified mt-3">
+            <li class="nav-item">
+                <a class="nav-link text-muted active"
+                    href="{{ route('users.show', ['name' => $user->name]) }}">
+                    記事
+                </a>
+            </li>
+            <li class="nav-item"> 
+                <a class="nav-link text-muted"
+                    href="">
+                    いいね
+                </a>
+            </li>
+        </ul>
+        @foreach($articles as $article)
+            @include('articles.card')
+        @endforeach
     </div>
 @endsection
