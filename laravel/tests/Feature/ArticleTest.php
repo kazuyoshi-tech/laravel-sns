dcd98e994c9262c1af26cb43a542678dd768dad5
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-07-24 16:07:27 +0900

    テスト記述のリファクタリング

--- laravel/tests/Feature/ArticleTest.php
+++ laravel/tests/Feature/ArticleTest.php
@@ -38,37 +38,36 @@ class ArticleTest extends TestCase
 
     public function testArticleIndex(): void
     {
-        $response = $this->get(route('articles.index'))
-            ->assertStatus(200)
-            ->assertSee($this->article->user->name)
-            ->assertSee($this->article->title)
-            ->assertSee($this->article->body);
+        $this->get(route('articles.index'))
+              ->assertStatus(200)
+              ->assertSee($this->article->user->name)
+              ->assertSee($this->article->title)
+              ->assertSee($this->article->body);
     }
 
     public function show(): void
     {
-        $response = $this->get(route('articles.show' ,['article' => $this->article]))
-            ->assertStatus(200)
-            ->assertSee($this->article->user->name)
-            ->assertSee($this->article->title)
-            ->assertSee($this->article->body);
+        $this->get(route('articles.show' ,['article' => $this->article]))
+              ->assertStatus(200)
+              ->assertSee($this->article->user->name)
+              ->assertSee($this->article->title)
+              ->assertSee($this->article->body);
     }
 
     public function testArticleCreate_Success(): void
     {
         // actingAsヘルパで現在認証済みのユーザーを指定する
-        $response = $this->actingAs($this->article->user);
- 
-        $response->get(route('articles.create'))
-            ->assertStatus(200);
+        $this->actingAs($this->article->user);
+              ->get(route('articles.create'))
+              ->assertStatus(200);
     }
 
     public function testArticleCreate_Fails(): void
     {
         #ユーザー認証なしで記事作成ページに行けない
-        $response = $this->get(route('articles.create'))
-            ->assertStatus(302)
-            ->assertRedirect('login');
+        $this->get(route('articles.create'))
+              ->assertStatus(302)
+              ->assertRedirect('login');
     }
 
     public function testArticleEdit_Success(): void
@@ -77,27 +76,27 @@ class ArticleTest extends TestCase
         $response = $this->actingAs($this->article->user);
 
         #記事作成ユーザーで記事更新ページに行ける
-        $response = $this->get(route('articles.edit', ['article' => $this->article]))
-            ->assertStatus(200)
-            ->assertSee($this->article->title)
-            ->assertSee($this->article->body);
+        $this->get(route('articles.edit', ['article' => $this->article]))
+              ->assertStatus(200)
+              ->assertSee($this->article->title)
+              ->assertSee($this->article->body);
     }
 
     public function testArticleEdit_Fail_Guest(): void
     {
         #ユーザー認証なしで記事更新ページに行けない
-        $response = $this->get(route('articles.edit', ['article' => $this->article]))
-            ->assertStatus(302)
-            ->assertRedirect('login');
+        $this->get(route('articles.edit', ['article' => $this->article]))
+              ->assertStatus(302)
+              ->assertRedirect('login');
     }
 
     public function testArticleEdit_Fail_OtherUser(): void
     {
         #アナザーユーザーの作成＋ログイン+アナザーユーザーでは許可されていない
         $this->otherUser = factory(User::class)->create();
-        $response = $this->actingAs($this->otherUser)
-            ->get(route('articles.edit', ['article' => $this->article]))
-            ->assertStatus(403);
+        $this->actingAs($this->otherUser)
+              ->get(route('articles.edit', ['article' => $this->article]))
+              ->assertStatus(403);
     }
 
     public function testArticleStore_Success(): void
@@ -105,30 +104,30 @@ class ArticleTest extends TestCase
         #アナザーユーザーの作成＋ログイン+記事を投稿
         $this->otherUser = factory(User::class)->create();
         $this->actingAs($this->otherUser)
-            ->post(route('articles.store'), $this->articleDate_succuss)
-            ->assertStatus(302)
-            ->assertSessionHas('flash_message', '投稿が完了しました')
-            ->assertRedirect(route('articles.index'));
+              ->post(route('articles.store'), $this->articleDate_succuss)
+              ->assertStatus(302)
+              ->assertSessionHas('flash_message', '投稿が完了しました')
+              ->assertRedirect(route('articles.index'));
     }
 
     public function testArticleStore_fail(): void
     {
         #アナザーユーザーの作成＋ログイン+bodyの記述がなく'flash_message'がない
         $this->otherUser = factory(User::class)->create();
-        $response = $this->actingAs($this->otherUser)
-            ->post(route('articles.store'), $this->articleDate_fail)
-            ->assertStatus(302)
-            ->assertSessionHas('flash_message', '') #メッセージがない
-            ->assertRedirect(route('articles.index'));
+        $this->actingAs($this->otherUser)
+              ->post(route('articles.store'), $this->articleDate_fail)
+              ->assertStatus(302)
+              ->assertSessionHas('flash_message', '') #メッセージがない
+              ->assertRedirect(route('articles.index'));
     }
 
     public function testArticleUpdate_Success(): void
     {
         #既に投稿している記事のユーザーでログイン
-        $response = $this->actingAs($this->article->user)
-            ->put(route('articles.update' ,['article' => $this->article], $this->articleDate_succuss))
-            ->assertStatus(302)
-            ->assertRedirect(route('articles.index'));
+        $this->actingAs($this->article->user)
+              ->put(route('articles.update' ,['article' => $this->article], $this->articleDate_succuss))
+              ->assertStatus(302)
+              ->assertRedirect(route('articles.index'));
         
     }
 
