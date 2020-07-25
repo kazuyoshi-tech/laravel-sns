b4c00d5bad387fcafac80af53d3eaa646479a3fa
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-07-25 15:55:55 +0900

    記事削除unitテスト実装

--- laravel/tests/Unit/ArticleTest.php
+++ laravel/tests/Unit/ArticleTest.php
@@ -14,47 +14,23 @@ class ArticleTest extends TestCase
 
     use RefreshDatabase;
 
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        // テスト記事
-        $this->article = factory(Article::class)->create();
-    }
-
     public function test_Can_Create_a_article()
     {
+        $this->article = factory(Article::class)->create();
+
         $find_article = DB::table('articles')->where('id', $this->article->id)->first();
 
         $this->assertEquals($find_article->title, $this->article->title);
+        $this->assertEquals($find_article->body, $this->article->body);
     }
 
+    public function test_Can_delete_a_article()
+    {
 
-    //     $product = 'apple';
-    //     $cover = UploadedFile::fake()->image('file.png', 600, 600);
+        $this->article = factory(Article::class)->create();
 
-    //     $params = [
-    //         'sku' => $this->faker->numberBetween(1111111, 999999),
-    //         'name' => $product,
-    //         'slug' => str_slug($product),
-    //         'description' => $this->faker->paragraph,
-    //         'cover' => $cover,
-    //         'quantity' => 10,
-    //         'price' => 9.95,
-    //         'status' => 1,
-    //     ];
+        $delete_article = DB::table('articles')->where('id', $this->article->id)->delete();
 
-    //     $product = new ProductRepository(new Product);
-    //     $created = $product->createProduct($params);
-
-    //     $this->assertInstanceOf(Product::class, $created);
-    //     $this->assertEquals($params['sku'], $created->sku);
-    //     $this->assertEquals($params['name'], $created->name);
-    //     $this->assertEquals($params['slug'], $created->slug);
-    //     $this->assertEquals($params['description'], $created->description);
-    //     $this->assertEquals($params['cover'], $created->cover);
-    //     $this->assertEquals($params['quantity'], $created->quantity);
-    //     $this->assertEquals($params['price'], $created->price);
-    //     $this->assertEquals($params['status'], $created->status);
-    // }
+        $this->assertDatabaseMissing('articles', ['id' => $this->article->id]);
+    }
 }
